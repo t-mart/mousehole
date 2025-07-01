ff70e0a98f2271543639f996b33ebf022da83f53
Tim Martin <tim@timmart.in>
2025-06-30 22:42:58 -0500

    pass button type prop

--- src/frontend/components/button.tsx
+++ src/frontend/components/button.tsx
@@ -4,10 +4,12 @@ import { cn } from "#frontend/lib/cn.ts";
 
 export function Button({
   className,
+  type,
   ...props
 }: ComponentPropsWithRef<"button">) {
   return (
     <button
+      type={type}
       className={cn(
         "flex items-center justify-center rounded-md bg-primary-1 py-2 px-4 font-bold transition-[transform,color,box-shadow] hover:scale-95 aria-invalid:hover:scale-none cursor-pointer aria-invalid:cursor-auto text-primary-foreground aria-invalid:bg-muted outline-none focus-visible:ring-ring/50 focus-visible:ring-3 border-primary-foreground",
         className
