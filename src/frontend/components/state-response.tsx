4ff61596e21f608594e2af396a5576be877024d2
Tim Martin <tim@timmart.in>
2025-07-01 17:45:29 -0500

    Remove "show"

--- src/frontend/components/state-response.tsx
+++ src/frontend/components/state-response.tsx
@@ -9,7 +9,7 @@ export function StateResponse({
     <Section>
       <details className="w-full">
         <summary className="cursor-pointer hover:text-primary-2">
-          Show Mousehole API Response
+          Mousehole API Response
         </summary>
         <div className="text-left max-h-30 overflow-auto p-2 border-2 rounded-md mt-2 w-full">
           <pre className="">
